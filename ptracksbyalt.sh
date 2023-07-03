34a81fdda6b61bcc433b64cd80e4c9f7f1392d54
nickapos <nickapos@oncrete.gr>
2023-07-03 19:08:49 +0100

    adding symlink

--- ptracksbyalt.sh
+++ ptracksbyalt.sh
@@ -2,6 +2,7 @@
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 TARGET_DIR=/home/$USER/oarc-adsb-screenshots/$TIMESTAMP
+SYMLINK=$TARGET_DIR/latest
 
 mkdir -p $TARGET_DIR
 
@@ -10,4 +11,10 @@ google-chrome-stable --enable-logging=/dev/null --headless --disable-gpu --disab
 google-chrome-stable --enable-logging=/dev/null --headless --disable-gpu --disable-3d-apis --screenshot=$TARGET_DIR/5to17k.png --virtual-time-budget=60000 "https://adsb.oarc.uk/?ptracks&filterAltMin=5500&filterAltMax=17500&zoom=5.5&hideSidebar&hideButtons&screenshot"
 google-chrome-stable --enable-logging=/dev/null --headless --disable-gpu --disable-3d-apis --screenshot=$TARGET_DIR/17to30k.png --virtual-time-budget=60000 "https://adsb.oarc.uk/?ptracks&filterAltMin=17500&filterAltMax=30000&zoom=5.5&hideSidebar&hideButtons&screenshot"
 google-chrome-stable --enable-logging=/dev/null --headless --disable-gpu --disable-3d-apis --screenshot=$TARGET_DIR/30kandup.png --virtual-time-budget=60000 "https://adsb.oarc.uk/?ptracks&filterAltMin=35000&zoom=5.5&hideSidebar&hideButtons&screenshot"
+
+if [ -L $SYMLINK ] ; then
+    rm $SYMLINK
+fi
+
+ln -s $TARGET_DIR $SYMLINK
 
