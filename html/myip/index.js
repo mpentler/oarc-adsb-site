3b9bc41f66e813711eb104bb05a8f759882069ae
Roger Howell <MysterAitch@users.noreply.github.com>
2023-09-23 01:53:44 +0100

    Update the MyIP page to work with the v4 API, displaying data about all Beast/MLAT records not just the first one.

--- html/myip/index.js
+++ html/myip/index.js
@@ -1,66 +1,173 @@
 // Defining async function
-async function getapidata() {
+async function getApiData(apiUrlIp, apiUrlFeedCount) {
 
     // First the user stats...
     // Storing response
-    const response1 = await fetch("https://adsb.oarc.uk/api/v3/myip");
+    const userStatsResponse = await fetch(apiUrlIp);
 
     // Storing data in form of JSON
-    var userdata = await response1.json();
-    showuserstats(userdata);
+    const userData = await userStatsResponse.json();
+    updateUserStats(userData);
 
     // Now site stats...
     // Storing response
-    const response2 = await fetch("https://adsb.oarc.uk/api/v3/feedcount");
+    const siteStatsResponse = await fetch(apiUrlFeedCount);
 
     // Storing data in form of JSON
-    var sitedata = await response2.json();
-    showsitestats(sitedata);
+    const siteData = await siteStatsResponse.json();
+    updateSiteStats(siteData);
 }
 
-// Calling that async function
-let intervalID = setInterval(() => {
-        getapidata();
-}, 10000);
 
+function secondsToDhms(seconds) {
+    seconds = Number(seconds);
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor(seconds % (3600 * 24) / 3600);
+    const m = Math.floor(seconds % 3600 / 60);
+    const s = Math.floor(seconds % 60);
 
-// Function to update both stats panels
-function showuserstats(data) {
-    //Beast data
-    if (data[0] == "No match!") {
-        document.getElementsByClassName("beastdot")[0].style.backgroundColor = "red";
-        document.getElementById("beaststats").hidden = true;
-        document.getElementById("beaststatus").innerHTML = "You are not feeding Beast data!";
-    } else {
-        document.getElementsByClassName("beastdot")[0].style.backgroundColor = "green";
-        document.getElementById("beaststats").hidden = false;
-        document.getElementById("beaststatus").innerHTML = "You are feeding Beast data!";
-        document.getElementById("bandwidth").innerHTML = data[0][2];
-        document.getElementById("msgsec").innerHTML = data[0][4];
-        document.getElementById("possec").innerHTML = data[0][5];
-        document.getElementById("latency").innerHTML = data[0][7];
-        document.getElementById("postotal").innerHTML = data[0][8];
+    const dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
+    const hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+    const mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
+    const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
+
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+/**
+ * Replaces sections of the UUID to obfuscate it.
+ * - Many folks will screenshot this page and share it.
+ * - The UUID identifies the feeder setup, and anybody else copying/using this value could be problematic.
+ */
+function getObfuscatedUuid(uuid) {
+    return uuid === null ?
+        'null' :
+        uuid.split('-')
+            .map((item, i) => (i < 1 || i > 3) ? item : '....')
+            .join('-');
+}
+
+/**
+ * Replaces sections of the IP address to obfuscate it.
+ * - Many folks will screenshot this page and share it.
+ * - The IP address is not particularly sensitive, but it can be PII
+ *   and many users will be hesitant to share it so let's proactively obfuscate it.
+ */
+function getObfuscatedUserIp(userIp) {
+    return userIp
+        .split('.')
+        .map((item, i) => i < 2 ? 'xxx' : item)
+        .join('.');
+}
+
+function statusRowForUuid(data, uuid) {
+
+    const beastData = data.beastData.filter(item => item.uuid === uuid);
+    let beastHtml = `
+    <div class="col-sm">
+      <div class="card">
+        <div class="card-header">
+          <h4><span class="statusdot inactive_feed beastdot"></span> Beast ADS-B</h4>
+        </div>
+        <div class="card-body">
+          <div id="beaststats">
+            <strong>No beast data for this UUID</strong>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+    if(beastData.length === 1) {
+        const beastEntry = beastData[0];
+
+        beastHtml = `
+    <div class="col-sm">
+      <div class="card">
+        <div class="card-header">
+          <h4><span class="statusdot active_feed beastdot"></span> Beast ADS-B</h4>
+        </div>
+        <div class="card-body">
+
+          <div id="beaststats">
+            <ul>
+              <li>Messages: <span id="msgsec">` + beastEntry.messagesPerSecond + `</span>/sec</li>
+              <li>Positions: <span id="possec">` + beastEntry.positionsPerSecond + `</span>/sec</li>
+              <li>Positions total: <span id="postotal">` + beastEntry.positionsTotal + `</span></li>
+              <li>Bandwidth: <span id="bandwidth">` + beastEntry.bandwidthKbps + `</span>kbit/s</li>
+              <li>Latency: <span id="latency">` + beastEntry.latencyMs + `</span>ms</li>
+              <li>Connection time: <span id="latency">` + secondsToDhms(beastEntry.connectionTimeSeconds) + `</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>`
     }
 
-    //MLAT data
-    if (data[1] == "No match!") {
-        document.getElementsByClassName("mlatdot")[0].style.backgroundColor = "red";
-        document.getElementById("mlatstats").hidden = true;
-        document.getElementById("mlatstatus").innerHTML = "You are not feeding MLAT data!";
-    } else {
-        document.getElementsByClassName("mlatdot")[0].style.backgroundColor = "green";
-        document.getElementById("mlatstats").hidden = false;
-        document.getElementById("mlatstatus").innerHTML = "You are feeding MLAT data!";
-        document.getElementById("mlatusername").innerHTML = data[1]["user"];
-        document.getElementById("mlatmsgrate").innerHTML = data[1]["message_rate"];
-        document.getElementById("mlatpeers").innerHTML = data[1]["peer_count"];
-        document.getElementById("mlattimeout").innerHTML = data[1]["bad_sync_timeout"];
-        document.getElementById("mlatoutlier").innerHTML = data[1]["outlier_percent"];
+    const mlatData = data.mlatData.filter(item => item.uuid === uuid);
+    let mlatHtml = `
+    <div class="col-sm">
+      <div class="card">
+        <div class="card-header">
+          <h4><span class="statusdot inactive_feed mlatdot"></span> MLAT</h4>
+        </div>
+        <div class="card-body">
+          <div id="mlatstats">
+            <strong>No MLAT data for this UUID</strong>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+    if(mlatData.length === 1) {
+        const mlatEntry = mlatData[0];
+
+        mlatHtml = `
+    <div class="col-sm">
+      <div class="card">
+        <div class="card-header">
+          <h4><span class="statusdot active_feed mlatdot"></span> MLAT</h4>
+        </div>
+        <div class="card-body">
+          <div id="mlatstats">
+            <ul>
+              <li>Name: <span id="mlatusername">` + mlatEntry.user + `</span></li>
+              <li>Messages: <span id="mlatmsgrate">` + mlatEntry.message_rate + `</span>/sec</li>
+              <li>Peers: <span id="mlatpeers">` + mlatEntry.peer_count + `</span></li>
+              <li>Timeout: <span id="mlattimeout">` + mlatEntry.bad_sync_timeout + `</span></li>
+              <li>Outlier %: <span id="mlatoutlier">` + mlatEntry.outlier_percent + `%</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>`;
     }
+
+    const obfuscatedUuid = getObfuscatedUuid(uuid);
+    const output = `
+  <div class="row gx-3">
+    <h3>UUID: <code>` + obfuscatedUuid + `</code></h3>
+    ` + beastHtml + `
+    ` + mlatHtml + `
+  </div>`;
+
+    return output;
+}
+
+
+// Function to update both user stats panels
+function updateUserStats(data) {
+    // Show user's IP
+    const userIp = data.ip;
+    const userIpObfuscated = getObfuscatedUserIp(userIp);
+    document.getElementById("userip").innerHTML = userIpObfuscated;
+
+    const statusRows = data.uuids.map(uuid => statusRowForUuid(data, uuid));
+    document.getElementById("status_rows").innerHTML = statusRows.join('\n');
+
 }
 
 // Function to update site feed count data
-function showsitestats(data) {
+function updateSiteStats(data) {
     document.getElementById("beastcount").innerHTML = data[0];
     document.getElementById("mlatcount").innerHTML = data[1];
 }
