f69b789c66d6674ac47a3969a2138726030aa447
Roger Howell <MysterAitch@users.noreply.github.com>
2023-09-26 22:40:40 +0100

    Make the emergencies page pretty with pagination and sorting and descriptive data values etc

--- html/emergencies/index.js
+++ html/emergencies/index.js
@@ -1,3 +1,73 @@
 function LinkFormatter(value, row, index) {
-  return '<a href="'+row.link+'">Follow</a>';
+    return '<a href="' + row.link + '">Follow</a>';
 }
+
+function callsignFormatter(value, row, index) {
+    return '<code>' + value + '</code>';
+}
+
+function icaoHexFormatter(value, row, index) {
+    return '<code>' + value + '</code>';
+}
+
+function squawkFormatter(value, row, index) {
+    var squawkDescriptions = {
+        7500: 'Unlawful interference',
+        7600: 'Lost communications',
+        7700: 'General emergency'
+    };
+
+    var description = squawkDescriptions[value];
+    if (description === undefined) {
+        return '<code>' + value + '</code>';
+    }
+    return '<code>' + value + '</code> ' + '<br/>' + '<small>' + description + '</small>';
+}
+
+function detectedFormatter(value, row, index) {
+    const detected = new Date(value);
+    return '<span title="' + value + '">' + detected.toLocaleString() + '</span>';
+}
+
+function lastSeenFormatter(value, row, index) {
+    const detected = new Date(row.alert_detected);
+    const lastSeen = new Date(value);
+
+    return '<span title="' + lastSeen + '">' + lastSeen.toLocaleString() + '<br/>' + ' ' + '<small>' + '(since first detection: ' + humanReadableTimeDuration(detected, lastSeen) + ')' + '</small>' + '</span>';
+}
+function alertStoppedFormatter(value, row, index) {
+    const detected = new Date(row.alert_detected);
+    const stopped = new Date(value);
+
+    return '<span title="' + stopped + '">' + value + '<br/>' + ' ' + '<small>' + '(since first detection: ' + humanReadableTimeDuration(detected, stopped) + ')' + '</small>' + '</span>';
+}
+
+// hh hours, mm minutes, ss seconds
+function humanReadableTimeDuration(start, end) {
+    const diffMs = end - start;
+    const diffHours = Math.floor(diffMs / 1000 / 60 / 60);
+    const diffMinutes = Math.floor(diffMs / 1000 / 60) - diffHours * 60;
+    const diffSeconds = Math.floor(diffMs / 1000) - diffHours * 60 * 60 - diffMinutes * 60;
+
+    let val = '';
+    if (diffHours > 0) {
+        val += diffHours + ' hours';
+    }
+
+    if (diffMinutes > 0) {
+        if (val.length > 0) {
+            val += ', ';
+        }
+        val += diffMinutes + ' minutes';
+    }
+
+    if (diffSeconds > 0 || val === '') {
+        if (val.length > 0) {
+            val += ', ';
+        }
+        val += diffSeconds + ' seconds';
+    }
+
+    return val;
+}
+
